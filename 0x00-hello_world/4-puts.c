627ebc1d3942ff50f0c40f3127625f83d9712f46
Faheem47 <fahjeed47@gmail.com>
2022-09-11 05:55:24 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
  * main - entry point of the program
  * main is executed when the program runs
  * main return always 0 (success)
+ * the return value for main is used to indicate how the program exited
  */
 int main(void)
 {
