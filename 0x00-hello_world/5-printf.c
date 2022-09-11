bf2c87f944b2500708c121fbb50a32c51e731dd7
Faheem47 <fahjeed47@gmail.com>
2022-09-11 07:07:59 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main - entry point of the program
- * return: 0 (means the program runs correctly)
+ * return void: 0 (means the program runs correctly)
  */
 int main(void)
 {	printf("with proper grammar, but the outcome is a piece of art,\n");
