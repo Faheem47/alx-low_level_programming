80c51726c9182a9b08a13c73d9273b15e7308c7b
Faheem47 <fahjeed47@gmail.com>
2022-09-11 06:41:19 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
  * main - entry point of the program
- * main is executed when the program runs 
+ * main is executed when the program runs
  * return: 0 (success)
  * the return value for main is used to indicate how the program exited
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return	(0);
+return(0);
 }
