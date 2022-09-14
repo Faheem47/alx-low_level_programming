5f430f6932ef7ab4ebf7dfc03adb3fbe3998bdb9
Faheem47 <fahjeed47@gmail.com>
2022-09-14 10:57:35 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - entry point of the program
+ * _putchar - writes the character c to stdout
+ * Return: on success 1
+ *         on error, -1 is returned, and errno is set appropriately
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{	_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+	return (0);
+}
